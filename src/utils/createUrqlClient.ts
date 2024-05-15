a39c363c1a0fe04b06615db9d818bf22091f829c
Carolina Josephik <carolina.moraesjosephik@gmail.com>
2024-05-15 17:48:23 -0300

    feat: add global error handling (for authentication)

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,5 +1,7 @@
-import { SSRExchange, createClient, fetchExchange } from "urql";
 import { cacheExchange } from "@urql/exchange-graphcache";
+import { NextUrqlClientConfig } from "next-urql";
+import { Exchange, fetchExchange } from "urql";
+import { pipe, tap } from 'wonka';
 import {
   LoginMutation,
   LogoutMutation,
@@ -8,7 +10,19 @@ import {
   RegisterMutation,
 } from "../gql/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
-import { NextUrqlClientConfig } from "next-urql";
+import Router from "next/router";
+
+export const errorExchange: Exchange = ({ forward }) => ops$ => {
+  return pipe(
+    forward(ops$),
+    tap((response) => {
+      console.log(response);
+      if (response.error && response.error?.message.toLowerCase().includes("not authenticated")) {
+        Router.replace("/login");
+      }
+    })
+  );
+};
 
 const mutationUpdates = {
   logout: (_result, _args, cache, _info) => {
@@ -63,6 +77,7 @@ const mutationUpdates = {
 export const createUrqlClient: NextUrqlClientConfig = (ssrExchange: any) => ({
   url: "http://localhost:4000/graphql",
   exchanges: [
+    errorExchange,
     cacheExchange({
       updates: {
         Mutation: mutationUpdates,
